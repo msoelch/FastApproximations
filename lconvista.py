Fixed normalization of updated D

lconvista.py
@@ -2,6 +2,7 @@ import numpy as np
 import theano
 import theano.tensor as T
 import gzip, cPickle
+import time
 from osdfutils import crino
 from theano.tensor.nnet.conv import conv2d as nconv
 from theano.tensor.signal.conv import conv2d as sconv
@@ -32,34 +33,32 @@ def lconvista(config, shrinkage):
     L = theano.shared(value=np.asarray(_L, dtype=theano.config.floatX), borrow=True, name="L")
     params = (D,theta,L)
 
+    #filter shape information for speed up of convolution
+    fs1 = _D.shape
+    fs2 = (fs1[1],fs1[0],fs1[2],fs1 [3])
+
     #need tensor4:s due to interface of nnet.conv.conv2d
     #X.shape = (btsz, singleton dimension, h, w)
-    X = T.tensor4('X')
+    X = T.tensor4('X',dtype=theano.config.floatX)
     #Z.shape = (btsz, n_filters, h+s-1, w+s-1)
-    Z = T.zeros(config['Zinit'])
-
+    Z = nconv(-X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2)#T.zeros(config['Zinit'],dtype=theano.config.floatX)
+    Z1 = Z
     def rec_error(X,Z,D):
         rec = nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=_D.shape) #rec.shape == X.shape
-        rec_error = T.mean(theano.map(lambda Xi, reci: T.sum((Xi - reci)**2) , sequences=[X,rec])[0]) #alternative: T.mean( T.sum( T.sum( (x - rec)**2 , axis=3 ) , axis=2 ) )
+        rec_error = 0.5*T.mean(theano.map(lambda Xi, reci: T.sum((reci-Xi)**2) , sequences=[X,rec])[0]) #alternative: T.mean( T.sum( T.sum( (x - rec)**2 , axis=3 ) , axis=2 ) )
         return rec_error
 
 
-#################### TODO
     for i in range(layers):
-        gradZ = nconv(nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=_D.shape) - X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full')
-        Z = shrinkage(Z - 1/L * gradZ, theta)
-        
-        
-        #Z = shrinkage(Z - 1/L * T.grad(rec_error(X,Z,D),Z), theta)
+        gradZ = nconv(nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=fs1) - X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2)
+        Z = shrinkage(Z - 1/L * gradZ, theta) 
+        #Z = shrinkage(Z - 1/L * T.grad(rec_error(X,Z,D),Z), theta) #this grad is 1/btsz smaller due to T.mean instead of T.sum in rec_error
 
 
-
-
-    
-    sparsity = T.mean(theano.map(lambda Zi: T.sum(T.abs_(Zi)),Z)[0])#alternative: mean also over filter index dimension (axis 1), not just axis 0
+    sparsity = 0#T.mean(theano.map(lambda Zi: T.sum(T.abs_(Zi)),Z)[0])#alternative: mean also over filter index dimension (axis 1), not just axis 0
     cost = rec_error(X,Z,D) + sp_lmbd * sparsity
 
-    return X, params, cost, Z
+    return X, params, cost, Z, L
 
 
 
@@ -86,8 +85,8 @@ if __name__ == '__main__':
     # SET LEARNING PARAMETERS
     ####################  
     epochs = 20
-    btsz = 100
-    lr = 1
+    btsz = 200
+    lr = 1.
     momentum = 0.9
     decay = 0.95
     batches = data.shape[0]/btsz
@@ -100,12 +99,13 @@ if __name__ == '__main__':
     ####################
     # INITIALIZE ALGORITHM PARAMETERS
     #################### 
+    n_filters = 20
+    filter_size = 5 #filters assumed to be square
+    layers = 2
+    lmbd = 0.1 # sparsity weight
+    
 
-    n_filters = 100
-    filter_size = 3 #filters assumed to be square
-
-
-    Dinit = {"shape": (n_filters,filter_size,filter_size), "variant": "normal", "std": 0.1}
+    Dinit = {"shape": (n_filters,filter_size,filter_size), "variant": "normal", "std": 0.5}
     D = crino.initweight(**Dinit)#np.random.randn(n_filters*s,s)#
     # normalize atoms (SQUARE patches) to 1
     D /= np.sqrt(np.asarray(map(lambda patch: (patch**2).sum(keepdims=True),D)))
@@ -115,27 +115,19 @@ if __name__ == '__main__':
     theta = 0.1 # potentially adjust to size of Z, keep it simple in the beginning
 
     L = 1.
-    lmbd = 1. # sparsity weight
-    
+
     zinit_shape = (btsz,n_filters,data[0,0].shape[0]+filter_size-1,data[0,0].shape[1]+filter_size-1)
+    Xshape = data[:btsz].shape
 
+    config = {"D": D, "theta": theta, "L": L, "lambda": lmbd, "Zinit": zinit_shape, "layers": layers, "Xshape": Xshape}
+    
 
-    layers = 10
-
-    config = {"D": D, "theta": theta, "L": L, "lambda": lmbd, "Zinit": zinit_shape, "layers": layers}
-    
-    
-    
-    
-    
-    
-    
     # normalize weights according to this config
     norm_dic = {"D": {"axis":1, "c": 1.}}
     # threshold theta should be at least some value
     thresh_dic = {"theta": {"thresh": 1e-2}}
 
-    x, params, cost, z = lconvista(config=config, shrinkage=crino.sh)
+    x, params, cost, z, gradZ = lconvista(config=config, shrinkage=crino.sh)
     grads = T.grad(cost, params)
 
     # training ...
@@ -146,7 +138,7 @@ if __name__ == '__main__':
     # ... normalize weights
 
 
-    updates[params[0]] = updates[params[0]] / T.sqrt(theano.map(lambda patch: (patch**2).sum(keepdims=True),updates[params[0]])[0])
+    updates[params[0]] = updates[params[0]] / T.sqrt(theano.map(lambda patch: (patch**2).sum(keepdims=True),updates[params[0]][0])[0])
     #updates = crino.norm_updt(params, updates, todo=norm_dic)
 
 
@@ -154,15 +146,22 @@ if __name__ == '__main__':
     updates = crino.max_updt(params, updates, todo=thresh_dic)
 
     train = theano.function([x], cost, updates=updates, allow_input_downcast=True)
-
+    #train = theano.function([x], gradZ  , updates=updates, allow_input_downcast=True)
 
     print 'Graph built. Learning...'
-
-
     sz = data.shape[0]
     for epoch in xrange(epochs):
         cost = 0
+        start = time.clock()
         for mbi in xrange(batches):
-            print "mbi", mbi
+            #print np.sum(train(data[mbi*btsz:(mbi+1)*btsz])**2)
+            #print '>>>>>>>>>>>>>>>>>'   
+         
             cost += btsz*train(data[mbi*btsz:(mbi+1)*btsz])
+            if (mbi+1) % 50 == 0:
+                print "mbi", mbi
+                end = time.clock()
+                print "Elapsed time for last 50 minibatches", np.around(end-start,decimals=2), "seconds."
+                start = time.clock()
+            
         print epoch, cost/sz