afe5ec52662109c4904e0dec35ed9385e970ad1b
Maximilian Soelch <m.soelch@tum.de>
2014-09-02 17:57:54 +0200

    scaled grad, better initialization of Z, small fixes

--- lconvista.py
+++ lconvista.py
@@ -24,6 +24,7 @@ def lconvista(config, shrinkage):
 
     layers = config['layers']
     sp_lmbd = config['lambda']
+    btsz = config['btsz']
       
     _D = config['D']
     D = theano.shared(value=np.asarray(_D, dtype=theano.config.floatX), borrow=True, name='D')
@@ -41,24 +42,25 @@ def lconvista(config, shrinkage):
     #X.shape = (btsz, singleton dimension, h, w)
     X = T.tensor4('X',dtype=theano.config.floatX)
     #Z.shape = (btsz, n_filters, h+s-1, w+s-1)
-    Z = nconv(-X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2)#T.zeros(config['Zinit'],dtype=theano.config.floatX)
-    Z1 = Z
-    def rec_error(X,Z,D):
-        rec = nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=_D.shape) #rec.shape == X.shape
-        rec_error = 0.5*T.mean(theano.map(lambda Xi, reci: T.sum((reci-Xi)**2) , sequences=[X,rec])[0]) #alternative: T.mean( T.sum( T.sum( (x - rec)**2 , axis=3 ) , axis=2 ) )
-        return rec_error
+    Z = 0.5*T.ones(config['Zinit'],dtype=theano.config.floatX)#nconv(-X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2)#T.zeros(config['Zinit'],dtype=theano.config.floatX)
 
 
     for i in range(layers):
-        gradZ = nconv(nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=fs1) - X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2)
+        gradZ = nconv( nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=fs1) - X ,
+                      D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2) / btsz
         Z = shrinkage(Z - 1/L * gradZ, theta) 
         #Z = shrinkage(Z - 1/L * T.grad(rec_error(X,Z,D),Z), theta) #this grad is 1/btsz smaller due to T.mean instead of T.sum in rec_error
 
+    #def rec_error(X,Z,D):
+    rec = nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=_D.shape) #rec.shape == X.shape
+    rec_error = 0.5*T.mean(theano.map(lambda Xi, reci: T.sum((reci-Xi)**2) , sequences=[X,rec])[0]) #alternative: T.mean( T.sum( T.sum( (x - rec)**2 , axis=3 ) , axis=2 ) )
+    #    return rec_error
 
-    sparsity = 0#T.mean(theano.map(lambda Zi: T.sum(T.abs_(Zi)),Z)[0])#alternative: mean also over filter index dimension (axis 1), not just axis 0
-    cost = rec_error(X,Z,D) + sp_lmbd * sparsity
+    
+    sparsity = T.mean(theano.map(lambda Zi: T.sum(T.abs_(Zi)),Z)[0])#alternative: mean also over filter index dimension (axis 1), not just axis 0
+    cost = rec_error + sp_lmbd * sparsity
 
-    return X, params, cost, Z, L
+    return X, params, cost, Z, rec, (rec_error,sparsity)
 
 
 
@@ -80,13 +82,12 @@ if __name__ == '__main__':
 
     mnist_f.close()
    
-     
     ####################
     # SET LEARNING PARAMETERS
     ####################  
     epochs = 20
-    btsz = 200
-    lr = 1.
+    btsz = 100
+    lr = 1
     momentum = 0.9
     decay = 0.95
     batches = data.shape[0]/btsz
@@ -95,16 +96,14 @@ if __name__ == '__main__':
     print "Batches per epoch", batches
     print
 
-
     ####################
     # INITIALIZE ALGORITHM PARAMETERS
     #################### 
-    n_filters = 20
+    n_filters = 100
     filter_size = 5 #filters assumed to be square
-    layers = 2
-    lmbd = 0.1 # sparsity weight
+    layers = 3
+    lmbd = 0.01 # sparsity weight
     
-
     Dinit = {"shape": (n_filters,filter_size,filter_size), "variant": "normal", "std": 0.5}
     D = crino.initweight(**Dinit)#np.random.randn(n_filters*s,s)#
     # normalize atoms (SQUARE patches) to 1
@@ -119,15 +118,18 @@ if __name__ == '__main__':
     zinit_shape = (btsz,n_filters,data[0,0].shape[0]+filter_size-1,data[0,0].shape[1]+filter_size-1)
     Xshape = data[:btsz].shape
 
-    config = {"D": D, "theta": theta, "L": L, "lambda": lmbd, "Zinit": zinit_shape, "layers": layers, "Xshape": Xshape}
+    config = {"D": D, "theta": theta, "L": L, "lambda": lmbd, "Zinit": zinit_shape, "layers": layers, "Xshape": Xshape, "btsz": btsz}
     
-
     # normalize weights according to this config
     norm_dic = {"D": {"axis":1, "c": 1.}}
     # threshold theta should be at least some value
     thresh_dic = {"theta": {"thresh": 1e-2}}
 
-    x, params, cost, z, gradZ = lconvista(config=config, shrinkage=crino.sh)
+
+    ####################
+    # BUILDING GRAPH
+    #################### 
+    x, params, cost, z, rec, es = lconvista(config=config, shrinkage=sh)
     grads = T.grad(cost, params)
 
     # training ...
@@ -145,23 +147,30 @@ if __name__ == '__main__':
     # ... make sure threshold is big enough
     updates = crino.max_updt(params, updates, todo=thresh_dic)
 
-    train = theano.function([x], cost, updates=updates, allow_input_downcast=True)
-    #train = theano.function([x], gradZ  , updates=updates, allow_input_downcast=True)
+    #train = theano.function([x], (x, params[0], cost, z, rec, es[0], es[1]), updates=updates, allow_input_downcast=True)
+    train = theano.function([x], (cost,es[0],es[1]), updates=updates, allow_input_downcast=True)
 
-    print 'Graph built. Learning...'
+    print 'Graph built.'
+
+    ####################
+    # LEARNING / TRAINING
+    #################### 
     sz = data.shape[0]
     for epoch in xrange(epochs):
         cost = 0
         start = time.clock()
         for mbi in xrange(batches):
-            #print np.sum(train(data[mbi*btsz:(mbi+1)*btsz])**2)
-            #print '>>>>>>>>>>>>>>>>>'   
-         
-            cost += btsz*train(data[mbi*btsz:(mbi+1)*btsz])
+            #x, params, cost, z, rec = train(data[mbi*btsz:(mbi+1)*btsz]) 
+            #x, params, newcost, z, rec, re, sp = train(data[mbi*btsz:(mbi+1)*btsz])
+            newcost, re, sp = train(data[mbi*btsz:(mbi+1)*btsz])
+            cost += btsz*newcost
+            #cost += btsz*train(data[mbi*btsz:(mbi+1)*btsz])
+            print re,sp
+            #print re, sp, (x**2).sum, (rec**2).sum, (z**2).sum()
             if (mbi+1) % 50 == 0:
                 print "mbi", mbi
                 end = time.clock()
                 print "Elapsed time for last 50 minibatches", np.around(end-start,decimals=2), "seconds."
                 start = time.clock()
-            
+
         print epoch, cost/sz
