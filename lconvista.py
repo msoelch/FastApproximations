0e601ebd46613ae77304f244d674aed2ebf31f63
Maximilian Soelch <m.soelch@tum.de>
2014-09-05 19:54:52 +0200

    Notebook added

--- lconvista.py
+++ lconvista.py
@@ -7,6 +7,9 @@ from osdfutils import crino
 from theano.tensor.nnet.conv import conv2d as nconv
 from theano.tensor.signal.conv import conv2d as sconv
 
+from mlp import LogisticRegression
+
+
 
 def sh(x, theta):
     """Simple shrinkage function.
@@ -32,7 +35,7 @@ def lconvista(config, shrinkage):
     theta = theano.shared(value=np.asarray(_theta, dtype=theano.config.floatX), borrow=True, name="theta")
     _L = config['L']
     L = theano.shared(value=np.asarray(_L, dtype=theano.config.floatX), borrow=True, name="L")
-    params = (D,theta,L)
+    params = [D,theta,L]
 
     #filter shape information for speed up of convolution
     fs1 = _D.shape
@@ -42,7 +45,9 @@ def lconvista(config, shrinkage):
     #X.shape = (btsz, singleton dimension, h, w)
     X = T.tensor4('X',dtype=theano.config.floatX)
     #Z.shape = (btsz, n_filters, h+s-1, w+s-1)
-    Z = 2*T.ones(config['Zinit'],dtype=theano.config.floatX)#nconv(-X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2)#T.zeros(config['Zinit'],dtype=theano.config.floatX)
+    #Z = 2*T.ones(config['Zinit'],dtype=theano.config.floatX)
+    Z = T.zeros(config['Zinit'],dtype=theano.config.floatX)
+    #Z = shrinkage( -0.05*nconv(-X , D[:,:,::-1,::-1].dimshuffle(1,0,2,3), border_mode='full', image_shape=config['Xshape'], filter_shape=fs2) / L, theta )
 
     def rec_error(X,Z,D):
         rec = nconv(Z,D,border_mode='valid',image_shape=config['Zinit'],filter_shape=_D.shape) #rec.shape == X.shape
@@ -71,13 +76,26 @@ def lconvista(config, shrinkage):
     #rec_error = 0.5*T.mean(theano.map(lambda Xi, reci: T.sum((reci-Xi)**2) , sequences=[X,rec])[0]) #alternative: T.mean( T.sum( T.sum( (x - rec)**2 , axis=3 ) , axis=2 ) )
     #    return rec_error
 
+    logRegressionLayer = LogisticRegression(input=Z.reshape((config['Zinit'][0],np.asarray(config['Zinit'][1:]).prod())), n_in=np.asarray(config['Zinit'][1:]).prod(), n_out=10)
+
+    negative_log_likelihood = logRegressionLayer.negative_log_likelihood
+    
+    errors = logRegressionLayer.errors
+
+    # the parameters of the model are the parameters of the two layer it is
+    # made out of
+    params = params + logRegressionLayer.params
+
+
     sparsity = T.mean(T.sum(T.sum(T.abs_(Z),axis=-1),axis=-1))
     #sparsity = T.mean(theano.map(lambda Zi: T.mean(theano.map(lambda Zij: T.sum(T.abs_(Zij)), Zi)[0]), Z)[0])
     #sparsity = T.mean(theano.map(lambda Zi: T.sum(T.abs_(Zi)),Z)[0])#alternative: mean also over filter index dimension (axis 1), not just axis 0
     re, rec = rec_error(X,Z,D)    
-    cost = re + sp_lmbd * sparsity
+    y = T.ivector('y')
+    cerror = errors(y)
+    cost = 0.1*re + 0.1*sp_lmbd * sparsity + 100*negative_log_likelihood(y)
 
-    return X, params, cost, Z, rec, (re,sparsity)
+    return X, y, params, cost, Z, rec, re ,sparsity, negative_log_likelihood(y), cerror
 
 
 
@@ -104,10 +122,12 @@ if __name__ == '__main__':
     ####################  
     epochs = 20
     btsz = 100
-    lr = .1
+    lr = 1.
     momentum = 0.9
     decay = 0.95
     batches = data.shape[0]/btsz
+    tbatches = test.shape[0]/btsz
+    vbatches = valid.shape[0]/btsz
     print "LConvISTA -- Learned Convolutional ISTA"
     print "Epochs", epochs
     print "Batches per epoch", batches
@@ -116,10 +136,10 @@ if __name__ == '__main__':
     ####################
     # INITIALIZE ALGORITHM PARAMETERS
     #################### 
-    n_filters = 100
+    n_filters = 36
     filter_size = 5 #filters assumed to be square
-    layers = 10
-    lmbd = .1 # sparsity weight
+    layers = 3
+    lmbd = .001 # sparsity weight
     
     Dinit = {"shape": (n_filters,filter_size,filter_size), "variant": "normal", "std": 0.5}
     D = crino.initweight(**Dinit)#np.random.randn(n_filters*s,s)#
@@ -128,7 +148,7 @@ if __name__ == '__main__':
     # reshape to four dimensions to match nnet.conv.conv2d
     D = D.reshape(1,n_filters,filter_size,filter_size)
     
-    theta = 0.166 # potentially adjust to size of Z, keep it simple in the beginning
+    theta = 0.0001#0.166 # potentially adjust to size of Z, keep it simple in the beginning
 
     L = 1.
 
@@ -140,13 +160,13 @@ if __name__ == '__main__':
     # normalize weights according to this config
     norm_dic = {"D": {"axis":1, "c": 1.}}
     # threshold theta should be at least some value
-    thresh_dic = {"theta": {"thresh": 1e-2}}
+    thresh_dic = {"theta": {"thresh": 1e-15}}
 
 
     ####################
     # BUILDING GRAPH
     #################### 
-    x, params, cost, z, rec, es = lconvista(config=config, shrinkage=sh)
+    x, y, params, cost, z, rec, re, spe , neglog, classe= lconvista(config=config, shrinkage=sh)
     grads = T.grad(cost, params)
 
     # training ...
@@ -165,10 +185,79 @@ if __name__ == '__main__':
     updates = crino.max_updt(params, updates, todo=thresh_dic)
 
     #train = theano.function([x], (x, params[0], cost, z, rec, es[0], es[1]), updates=updates, allow_input_downcast=True)
-    train = theano.function([x], (cost,es[0],es[1],params[1],params[2]), updates=updates, allow_input_downcast=True)
-
+    train = theano.function([x,y], (cost,re,spe,params[1],params[2], neglog, classe), updates=updates, allow_input_downcast=True)
+    validate_or_test_model = theano.function([x,y], classe, allow_input_downcast=True)
     print 'Graph built.'
 
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print '... training'
+
+    # early-stopping parameters
+    patience = 1000  # look as this many examples regardless
+    patience_increase = 2  # wait this much longer when a new best is
+                           # found
+    improvement_threshold = 0.995  # a relative improvement of this much is
+                                   # considered significant
+    validation_frequency = min(batches/2, patience / 2)
+    print validation_frequency
+                                  # go through this many
+                                  # minibatche before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
+
+    best_params = None
+    best_validation_loss = np.inf
+    best_iter = 0
+    test_score = 0.
+    start_time = time.clock()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < epochs) and (not done_looping):
+        epoch += 1
+        for mbi in xrange(batches):
+            if (mbi+1) % 50 == 0: print mbi
+            newcost, re, sp, theta, L, neglog, cerror = train(data[mbi*btsz:(mbi+1)*btsz],dtrgts[mbi*btsz:(mbi+1)*btsz])
+            cost += btsz*newcost
+            # iteration number
+            iter = (epoch - 1) * batches + mbi
+
+            if (iter + 1) % validation_frequency == 0:
+                # compute zero-one loss on validation set
+                validation_losses = [validate_or_test_model(valid[i*btsz:(i+1)*btsz],vtrgts[i*btsz:(i+1)*btsz]) for i in xrange(vbatches)]
+                this_validation_loss = np.mean(validation_losses)
+
+                print('epoch %i, minibatch %i/%i, validation error %f %%' % (epoch, mbi + 1, batches, this_validation_loss * 100.))
+
+
+                if this_validation_loss < best_validation_loss:
+                    #improve patience if loss improvement is good enough
+                    if this_validation_loss < best_validation_loss * improvement_threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    # test it on the test set
+                    test_losses = [validate_or_test_model(test[i*btsz:(i+1)*btsz],ttrgts[i*btsz:(i+1)*btsz]) for i in xrange(tbatches)]
+                    test_score = np.mean(test_losses)
+
+                    print(('     epoch %i, minibatch %i/%i, test error of '
+                           'best model %f %%') %
+                          (epoch, mbi + 1, batches,
+                           test_score * 100.))
+
+            if patience <= iter:
+                    done_looping = True
+                    break
+
+
+
+    """
     ####################
     # LEARNING / TRAINING
     #################### 
@@ -179,10 +268,10 @@ if __name__ == '__main__':
         for mbi in xrange(batches):
             #x, params, cost, z, rec = train(data[mbi*btsz:(mbi+1)*btsz]) 
             #x, params, newcost, z, rec, re, sp = train(data[mbi*btsz:(mbi+1)*btsz])
-            newcost, re, sp, theta, L = train(data[mbi*btsz:(mbi+1)*btsz])
+            newcost, re, sp, theta, L, neglog, cerror = train(data[mbi*btsz:(mbi+1)*btsz],dtrgts[mbi*btsz:(mbi+1)*btsz])
             cost += btsz*newcost
             #cost += btsz*train(data[mbi*btsz:(mbi+1)*btsz])
-            print re,sp, np.asarray(theta), np.asarray(L)
+            print re,sp, np.asarray(theta), np.asarray(L), neglog, cerror
             #print re, sp, (x**2).sum, (rec**2).sum, (z**2).sum()
             if (mbi+1) % 50 == 0:
                 print "mbi", mbi
@@ -191,3 +280,5 @@ if __name__ == '__main__':
                 start = time.clock()
 
         print epoch, cost/sz
+
+    """
